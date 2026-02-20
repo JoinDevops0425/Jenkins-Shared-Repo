7638f0c8e7eb3370852585cb79b827ed1d563c91
Uday <JoinDevops0425@gmail.com>
2026-02-20 23:16:57 +0530

    Fix Docker build command syntax

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 def call(String user_name, String image_name){
 echo 'building it'
-sh """docker build -t "${user_name}"/"${image_name}":latest .""" 
+sh """docker build -t ${user_name}/${image_name}:latest .""" 
 }
